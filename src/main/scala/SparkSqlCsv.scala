aa7efda30182b6bf8f5334b2adb8ba5e699ca1dc
eugenelazeba <ela@ciklum.com>
2016-10-13 16:29:34 +0300

    small fix with value namesV

--- src/main/scala/SparkSqlCsv.scala
+++ src/main/scala/SparkSqlCsv.scala
@@ -3,10 +3,11 @@
   */
 
 import org.apache.spark.{SparkConf, rdd}
-import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode}
+import org.apache.spark.sql._
 import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{DataFrame, DataFrameWriter, Row}
+
+import scala.collection.immutable.Range.Inclusive
 
 
 
@@ -29,25 +30,25 @@ object SparkSqlCsv {
     /** First task
       * List of all airports with total number of planes for the whole period that arrived to each airport
       * */
-    val dfTaskFirst = sqlc.sql("SELECT (DEST), count(*) FROM MY group by (DEST)")
-
-    dfTaskFirst.repartition(1).write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "false").save("task1")
-
-
-    /** Second task
-      * Non-Zero difference in total number of planes that arrived to and left from the airport
-      * */
-    val dfGroupedByOriginMinus = sqlc.sql("SELECT (ORIGIN), -count(*) FROM MY group by (ORIGIN)")
-    val dfGroupedByDestination = sqlc.sql("SELECT (DEST), count(*) FROM MY group by (DEST)")
-    val dfUnionDestOrigin = dfGroupedByDestination.union(dfGroupedByOriginMinus)
-    val dfSumUnionDestOrigin = dfUnionDestOrigin.groupBy("DEST").sum("count(1)")
-
-    val setNameCountColumn = Seq("DEST", "QUANTITY")
-    val dfTaskSecondNotFiltered = dfSumUnionDestOrigin.toDF(setNameCountColumn: _*)
-
-    val dfTaskSecond = dfTaskSecondNotFiltered.filter("QUANTITY !=0")
-
-    dfTaskSecond.coalesce(1).write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "false").save("task2")
+//    val dfTaskFirst = sqlc.sql("SELECT (DEST), count(*) FROM MY group by (DEST)")
+//
+//    dfTaskFirst.repartition(1).write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "false").save("task1")
+//
+//
+//    /** Second task
+//      * Non-Zero difference in total number of planes that arrived to and left from the airport
+//      * */
+//    val dfGroupedByOriginMinus = sqlc.sql("SELECT (ORIGIN), -count(*) FROM MY group by (ORIGIN)")
+//    val dfGroupedByDestination = sqlc.sql("SELECT (DEST), count(*) FROM MY group by (DEST)")
+//    val dfUnionDestOrigin = dfGroupedByDestination.union(dfGroupedByOriginMinus)
+//    val dfSumUnionDestOrigin = dfUnionDestOrigin.groupBy("DEST").sum("count(1)")
+//
+//    val setNameCountColumn = Seq("DEST", "QUANTITY")
+//    val dfTaskSecondNotFiltered = dfSumUnionDestOrigin.toDF(setNameCountColumn: _*)
+//
+//    val dfTaskSecond = dfTaskSecondNotFiltered.filter("QUANTITY !=0")
+//
+//    dfTaskSecond.coalesce(1).write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "false").save("task2")
 
     /** Third task
       * Do the point 1 but sum number of planes separately per each week
@@ -59,11 +60,16 @@ object SparkSqlCsv {
     val lastWeekInYear = sqlc.sql("SELECT (FL_DATE) FROM MY")
     val lastWeekInYearSorted = lastWeekInYear.sort(desc("FL_DATE"))
 
-    for  (i<-1 to lastWeekInYearSorted.first().getInt(0)) {
-            getWeek(sqlc, "FL_DATE="+i, dfOriginDestFlDateWeeks)}
+    val weeks: Inclusive = 1 to lastWeekInYearSorted.first().getInt(0)
+    weeks
+      .map(i => getWeek(sqlc, "FL_DATE="+i, dfOriginDestFlDateWeeks))
+      .foreach(printToFile)
 
+    def printToFile(dataset: Dataset[Row]): Unit = {
+      dataset.repartition(1).write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "false").save("task3")
+    }
 
-    def getWeek(sqlc: SQLContext, week: String, dfOriginDestFlDateWeeks: DataFrame): Unit = {
+    def getWeek(sqlc: SQLContext, week: String, dfOriginDestFlDateWeeks: DataFrame) = {
       val dfOriginDestFlDateWeek = dfOriginDestFlDateWeeks.filter(week)
 
       dfOriginDestFlDateWeek.createOrReplaceTempView("MY")
@@ -71,9 +77,7 @@ object SparkSqlCsv {
       val weekDest = dfOriginDestFlDateWeek.select("DEST").groupBy("DEST").count()
       val weekOrigin = sqlc.sql("SELECT (ORIGIN), -count(*) FROM MY group by (ORIGIN)")
       val dfTaskThird = weekDest.union(weekOrigin.withColumn("(-count(1))", lit(0)))
-
-      dfTaskThird.repartition(1).write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "false").save("task3")
-
+      dfTaskThird
     }
   }
 
